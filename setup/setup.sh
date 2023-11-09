d02b02668af0f0d5edd7ffa108e5931a2da9c7d6
Alex Turner <turneral1@grinnell.edu>
2023-11-09 10:51:29 -0800

    Installed neovim, and checks before overwriting existing *rc files

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,9 +6,33 @@ if [ "$(pwd)" != "$HOME/.dotfiles" ]; then
   cd "$HOME/.dotfiles" || exit  # Exit if the directory change fails
 fi
 
-# Use brace expansion to create links to .bashrc, .vimrc, and .netrc in the home directory
-ln "$PWD"/.{bash,vim}rc "$HOME"
-ln "$PWD"/.gitconfig "$HOME"
+# Install neovim
+if [ "$(uname)" == "Darwin" ]; then
+  brew install neovim
+else
+  sudo apt-get update
+  sudo apt-get install neovim
+fi
+
+# Link .bashrc, .vimrc, and .gitconfig to the home directory, with warnings for existing files
+for file in .bashrc .vimrc .gitconfig; do
+  if [ -e "$HOME/$file" ]; then
+    # Prompt the user to confirm overwriting the existing file
+    read -p "$file already exists. Overwrite? (y/N) " choice
+    case "$choice" in
+      y|Y ) ln -f "$PWD/$file" "$HOME";;
+      * ) echo "Skipping $file";;
+    esac
+  else
+    ln "$PWD/$file" "$HOME"
+  fi
+done
+
+# Create neovim settings which include current vimrc files
+NEOVIM_CONFIG_DIR="$HOME/.config/nvim/"
+mkdir -p "$NEOVIM_CONFIG_DIR"
+# Only append this line if it isn't already present in neovim's init
+grep -qxF "source $PWD/.vimrc" "$NEOVIM_CONFIG_DIR/init.vim" || echo "source $PWD/.vimrc" >> "$NEOVIM_CONFIG_DIR/init.vim"
 
 # Use brace expansion to ensure the extras files exist in the home directory
 touch "$HOME"/.{bashrc,fish_config}_extras
