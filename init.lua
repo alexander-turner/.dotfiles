49caf48d9b6f10cbd7a6b464c1a6f3166da5374e
Alex Turner <turneral1@grinnell.edu>
2023-12-27 12:29:32 -0800

    Fixed Comment functionality

--- init.lua
+++ init.lua
@@ -1,19 +1,16 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
 -- Include vimrc settings
 vim.cmd("source ~/.vimrc")
 
 return {
-  { "numToStr/Comment.nvim", lazy = false, },
 
-  -- add gruvbox
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-
-  -- Configure LazyVim to load gruvbox
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin-latte",
-    },
-  },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "catppuccin-latte",
+		},
+	},
 }
