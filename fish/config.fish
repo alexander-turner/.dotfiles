43117f1c252d1a6e0c4128f9c84f6fa6dfb10fb2
Alex Turner <turneral1@grinnell.edu>
2024-01-19 16:01:37 -0800

    Fixed logic issue with tmux starting

--- fish/config.fish
+++ fish/config.fish
@@ -34,7 +34,6 @@ end
 fish_vi_key_bindings
 
 if status is-interactive
-    # don't nest inside another tmux
     and not set -q TMUX
     # Create session 'main' or attach to 'main' if already exists.
     tmux new-session -A -s main
