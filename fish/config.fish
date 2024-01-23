a4826ad9da1d7b0418b0a3e5328e98439f52c501
Alex Turner <turneral1@grinnell.edu>
2024-01-23 01:43:24 +0000

    Added tmux copying ability, removed tmux auto start on fish shell interactive mode

--- fish/config.fish
+++ fish/config.fish
@@ -38,11 +38,11 @@ end
 # Custom settings
 fish_vi_key_bindings
 
-if status is-interactive
-    and not set -q TMUX
-    # Create session 'main' or attach to 'main' if already exists.
-    tmux new-session -A -s main
-end
+# if status is-interactive
+#     and not set -q TMUX
+#     # Create session 'main' or attach to 'main' if already exists.
+#     tmux new-session -A -s main
+# end
 
 # Custom functions
 function compress
