09af32704cba31479bd0f7e12c77d850c2631c29
Alex Turner <turneral1@grinnell.edu>
2024-03-06 10:34:06 -0800

    Resolved vim issue within tmux, and started tmux later in fish config so that PATH has time to be updated.

--- apps/fish/config.fish
+++ apps/fish/config.fish
@@ -33,12 +33,6 @@ else
     end
 end
 
-if status is-interactive
-    and not set -q TMUX
-    # Create session 'main' or attach to 'main' if already exists.
-    tmux new-session -A -s main
-end
-
 # Custom functions
 function compress # TODO pull compress
     ~/bin/media_upload/compress.sh
@@ -207,3 +201,9 @@ function fxtra
 end
 
 test -e {$HOME}/.iterm2_shell_integration.fish; and source {$HOME}/.iterm2_shell_integration.fish
+
+if status is-interactive
+    and not set -q TMUX
+    # Create session 'main' or attach to 'main' if already exists.
+    tmux new-session -A -s main
+end
