270c37d463012ac22ca8bc91a233bef689a027ce
Alex Turner <turneral1@grinnell.edu>
2024-03-19 21:59:10 -0700

    Updated nvim to use consistent github cmp hotkeys

--- apps/nvim/lua/plugins/cmp.lua
+++ apps/nvim/lua/plugins/cmp.lua
@@ -2,52 +2,23 @@ local cmp = require("cmp")
 
 cmp.setup({
   -- You can set other general options here
-  mapping = {
-    -- Other key mappings for nvim-cmp
-    ["<CR>"] = function(fallback)
-      fallback() -- This will just execute the default action, which is typically inserting a newline
-    end,
-
-    -- Override the Shift+Tab key to accept the current suggestion
-    ["<S-Tab>"] = cmp.mapping.confirm({ select = true }),
-
-    -- Example of mapping the Tab key to select the next item
-    ["<Tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end,
-  },
-
   -- Configure sources and other settings
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
+    { name = "copilot", group_index = 2 },
+    {
+      name = "zbirenbaum/copilot-cmp",
+      config = function()
+        require("copilot_cmp").setup()
+      end,
+    },
   }),
+  mapping = {
+    ["<tab>"] = cmp.mapping.confirm({ select = true }),
+    ["<CR>"] = function(fallback)
+      fallback()
+    end,
+  },
 })
 
 return { "cmp-tab" }
-
--- return {
---   {
---     "cmp-tab",
---     keys = { -- Other key mappings for nvim-cmp
---       ["<CR>"] = function(fallback)
---         fallback() -- This will just execute the default action, which is typically inserting a newline
---       end,
---
---       -- Override the Shift+Tab key to accept the current suggestion
---       ["<S-Tab>"] = cmp.mapping.confirm({ select = true }),
---
---       -- Example of mapping the Tab key to select the next item
---       ["<Tab>"] = function(fallback)
---         if cmp.visible() then
---           cmp.select_next_item()
---         else
---           fallback()
---         end
---       end,
---     },
---   },
--- }
