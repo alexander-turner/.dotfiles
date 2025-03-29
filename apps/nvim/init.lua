34a67e27f1576940854f83a9ce2f60908b9138c3
Alex Turner <alex@turntrout.com>
2025-03-29 09:52:32 -0700

    Nvim now has Fish LSP

--- apps/nvim/init.lua
+++ apps/nvim/init.lua
@@ -65,6 +65,17 @@ require("conform").setup({
   },
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "fish",
+  callback = function()
+    vim.lsp.start({
+      name = "fish-lsp",
+      cmd = { "fish-lsp", "start" },
+      cmd_env = { fish_lsp_show_client_popups = false },
+    })
+  end,
+})
+
 return {
 
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
