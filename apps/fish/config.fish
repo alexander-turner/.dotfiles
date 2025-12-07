b33e45c78fe4ae799711dee43e72252a5bdfd130
Alex Turner <turneral1@grinnell.edu>
2025-12-07 14:29:08 -0800

    fix: added pnpm config

--- apps/fish/config.fish
+++ apps/fish/config.fish
@@ -254,3 +254,10 @@ function aider_redpill
 end
 
 set -x OLLAMA_ORIGINS *
+
+# pnpm
+set -gx PNPM_HOME "/root/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
