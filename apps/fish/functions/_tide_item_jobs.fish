18487c07b771d5fde28d462d5270dfa980c0f4bc
Alex Turner <turneral1@grinnell.edu>
2025-12-07 15:20:14 -0800

    tide item job check

--- apps/fish/functions/_tide_item_jobs.fish
+++ apps/fish/functions/_tide_item_jobs.fish
@@ -1,3 +1,7 @@
 function _tide_item_jobs
-    set -q _tide_jobs && _tide_print_item jobs $tide_jobs_icon
+    if set -q _tide_jobs
+        if test -n "$_tide_jobs" -a "$_tide_jobs" -ge 0 2>/dev/null
+            _tide_print_item jobs $tide_jobs_icon
+        end
+    end
 end
