22c081d4c134c14faa6c02672382a98daaab7d44
Alex Turner <turneral1@grinnell.edu>
2025-12-08 21:13:17 -0800

    fix: moved NeoVim plugins from the init file to their own Lua files

--- apps/nvim/lua/plugins/conform.lua
+++ apps/nvim/lua/plugins/conform.lua
@@ -1,4 +1,12 @@
 return {
   "stevearc/conform.nvim",
-  opts = {},
+  opts = {
+    formatters_by_ft = {
+      fish = { "fish_indent" },
+    },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
+  },
 }
