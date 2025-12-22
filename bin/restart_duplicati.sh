673d13092269d28b0b18d48cf683c065ec578f43
Alex Turner <turneral1@grinnell.edu>
2025-12-21 17:19:58 -0800

    fix: no longer passing in blank encryption key to duplicati

--- bin/restart_duplicati.sh
+++ bin/restart_duplicati.sh
@@ -3,4 +3,4 @@
 
 sudo pkill -9 -f duplicati-server
 sleep 2
-sudo envchain duplicati /Applications/Duplicati.app/Contents/MacOS/duplicati-server --webservice-port 8200 --webservice-interface any --server-datafolder /Users/Shared/Duplicati/data --log-file=/Users/Shared/Duplicati/log/duplicati.log --log-level Warning --server-encryption-key &
+sudo envchain duplicati /Applications/Duplicati.app/Contents/MacOS/duplicati-server --webservice-port 8200 --webservice-interface any --server-datafolder /Users/Shared/Duplicati/data --log-file=/Users/Shared/Duplicati/log/duplicati.log --log-level Warning &
